74a102b25e8d5b8e7f6462926877b326bb0039e5
bitscuit <li_zhao_bin@hotmail.com>
2018-06-14 19:02:01 -0400

    working firebase call

--- src/main/java/com/qhackers/djcloud/Main.java
+++ src/main/java/com/qhackers/djcloud/Main.java
@@ -1,9 +1,12 @@
 package com.qhackers.djcloud;
 
 import java.io.FileInputStream;
-import com.google.firebase.FirebaseOptions;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -11,25 +14,32 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("hello");
-        FileInputStream serviceAccount = new FileInputStream("dj-cloud-fdbf5-firebase-adminsdk-mjsrn-b1c015f95b.json");
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("hello world");
+        FileInputStream serviceAccount = new FileInputStream("./dj-cloud-fdbf5-firebase-adminsdk-mjsrn-b1c015f95b.json");
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setDatabaseUrl("https://dj-cloud-fdbf5.firebaseio.com").build();
         FirebaseApp.initializeApp(options);
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("admin");
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Object document = dataSnapshot.getValue();
-                System.out.println(document);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-            }
+        	
+		    @Override
+			public void onDataChange(DataSnapshot snapshot) {
+				Object doc = snapshot.getValue();
+				System.out.println(doc);
+			}
+			
+			@Override
+			public void onCancelled(DatabaseError error) {
+				// TODO Auto-generated method stub
+				
+			}
         });
+        long counter = 0;
         System.out.println("end");
-    }
+        new CountDownLatch(1).await(5L, TimeUnit.SECONDS);
+	}
+
 }
